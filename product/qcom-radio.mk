78cde9bfbf7f55d1b7190597691f12da1ef45c81
dev-harsh1998 <harshitjain6751@gmail.com>
2017-04-15 21:17:38 +0530

    a6000: both sims could be LTE

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,6 +8,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     persist.radio.custom_ecc=1 \
     persist.radio.ecc_hard_1=112,911,110,122,119,120,000,118 \
+    ro.telephony.default_network=9,9 \
     persist.radio.ecc_hard_count=1 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
     ril.subscription.types=RUIM
