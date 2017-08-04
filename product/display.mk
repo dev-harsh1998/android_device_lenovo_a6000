d25c89ea8b7fa9eba1b12011e38ca8005d1e3f83
dev-harsh1998 <harshit.india@linaro.org>
2017-08-04 19:29:39 +0200

    a6000-64: Stop building libjni-livedisplay
    *Fuck away cm stuff for now

--- product/display.mk
+++ product/display.mk
@@ -2,10 +2,6 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Panel
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
-
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
