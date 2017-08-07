671bce31e175372668af6d8c60bcbf69c52726e9
karthick111 <karthickrko61@gmail.com>
2017-08-07 13:48:16 +0200

    a6000-64: AOSP-ify

---
+++ AndroidProducts.mk
@@ -0,0 +1,1 @@
+PRODUCT_MAKEFILES := device/lenovo/a6000/aosp_a6000.mk
--- aosp_a6000.mk
+++ aosp_a6000.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, device/lenovo/a6000/full_a6000.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/hyper/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -23,7 +23,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
 
-PRODUCT_NAME := lineage_a6000
+PRODUCT_NAME := aosp_a6000
 BOARD_VENDOR := Lenovo
 
 # Build fingerprint
