d6549d305296f4bef4e1d17c55e96c33f9f22e96
dev-harsh1998 <harshit.india@linaro.org>
2017-09-06 01:05:18 +0530

    a6000-64: inherit 2048 hwui props
    * Also disable livedisplay build for now
    * 2048 beause phone dosent boots up with 1024 one

--- product/display.mk
+++ product/display.mk
@@ -3,8 +3,8 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Panel
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
+#PRODUCT_PACKAGES += \
+#    libjni_livedisplay
 
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -14,15 +14,18 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 #HWUI Props
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=24 \
-    ro.hwui.layer_cache_size=16 \
-    ro.hwui.r_buffer_cache_size=2 \
-    ro.hwui.path_cache_size=4 \
-    ro.hwui.gradient_cache_size=0.5 \
-    ro.hwui.drop_shadow_cache_size=2 \
-    ro.hwui.texture_cache_flushrate=0.6 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=256 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=512
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.hwui.texture_cache_size=24 \
+#    ro.hwui.layer_cache_size=16 \
+#    ro.hwui.r_buffer_cache_size=2 \
+#    ro.hwui.path_cache_size=4 \
+#    ro.hwui.gradient_cache_size=0.5 \
+#    ro.hwui.drop_shadow_cache_size=2 \
+#    ro.hwui.texture_cache_flushrate=0.6 \
+#    ro.hwui.text_small_cache_width=1024 \
+#    ro.hwui.text_small_cache_height=256 \
+#    ro.hwui.text_large_cache_width=2048 \
+#    ro.hwui.text_large_cache_height=512
+
+# Inherit 2048 hwui props for now
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
