fb2ad150562fc88cca6c657e315c3049d41fb65f
dev-harsh1998 <harshit.india@linaro.org>
2017-12-28 12:44:55 +0100

    a6000: Camera from a6020

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,21 +1,11 @@
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
-    libmm-qcamera \
+    libboringssl-compat \
+    libbson \
+    libshim_gui \
     Snap
 
-# Build libstlport for vendor blobs
+# Stlport
 PRODUCT_PACKAGES += \
     libstlport
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0 \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
