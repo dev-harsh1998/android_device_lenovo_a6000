48b579180c6b08c28de7de91df2423d7becc6693
dev-harsh1998 <harshit.india@linaro.org>
2018-02-18 18:05:33 +0530

    a6000: init: Make 1GB ram devices GO

--- product/perf.mk
+++ product/perf.mk
@@ -5,7 +5,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.trim_empty_percent=100 \
     ro.sys.fw.trim_cache_percent=100 \
     ro.sys.fw.trim_enable_memory=1073741824 \
-    dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapminfree=6m \
     dalvik.vm.heapstartsize=14m \
     dalvik.vm.heapsize=256m \
