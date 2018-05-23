4b61ef931db612bc62db4b15864130e32a30c8ec
Kyle Harrison <khwebmail@gmail.com>
2018-05-23 23:37:30 +0530

    a6000: Build new GPS implementation
    Signed-off-by: dev-harsh1998 <harshit.india@linaro.org>

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,7 +1,21 @@
 # GPS
 PRODUCT_PACKAGES += \
+    libcurl \
+    libgnss \
+    libgnsspps
+
+# GPS-HIDL
+PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-service-qti
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/vendor/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/vendor/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/vendor/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/vendor/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/vendor/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
 # Permissions
 PRODUCT_COPY_FILES += \
