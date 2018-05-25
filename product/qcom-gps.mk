5f88beb32ae9532ad8d6ded31b33062b79c35eb0
Kyle Harrison <khwebmail@gmail.com>
2018-05-25 18:00:20 +0100

    a6000: Remove current GPS implementation
    * Includes the following commit: chil360/android_device_motorola_msm8916-common@90e97b86edff674653669a17955abe7240d2124b
    
    Signed-off-by: dev-harsh1998 <harshit.india@linaro.org>

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,16 +1,7 @@
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
-    gps.msm8916 \
-    libshims_flp \
-    libshims_get_process_name
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    android.hardware.gnss@1.0-service
 
 # Permissions
 PRODUCT_COPY_FILES += \
