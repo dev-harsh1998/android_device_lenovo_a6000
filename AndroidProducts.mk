498cded6bf6ce105dd6a33d2321fc84535459342
dev-harsh1998 <dev-harsh1998@hotmail.com>
2019-10-03 14:15:31 +0530

    a6000: add lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,9 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_a6000.mk \
     $(LOCAL_DIR)/aicp_a6000.mk
+
+# Lunch choices
+COMMON_LUNCH_CHOICES := \
+    aosp_a6000-userdebug \
+    aosp_a6000-eng \
+    aosp_a6000-user
