ad68595f24a39d4195af9b27461de15cd4f5fd7d
Jaroslav Zviezda <acroreiser@gmail.com>
2020-01-23 08:37:38 +0300

    init: switch to a6010 init

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -34,7 +34,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/sysinfo.h>
-
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
@@ -81,26 +80,47 @@ static void init_alarm_boot_properties()
     }
 }
 
-bool is2GB()
+int is2GB()
 {
     struct sysinfo sys;
     sysinfo(&sys);
     return sys.totalram > 1024ull * 1024 * 1024;
 }
 
-void set_device_dalvik_properties()
-{
-  property_set("dalvik.vm.heapstartsize", "16m");
-  property_set("dalvik.vm.heapgrowthlimit", is2GB() ? "256m" : "128m");
-  property_set("setprop dalvik.vm.heapsize", is2GB() ? "512m" : "256m");
-  property_set("dalvik.vm.heaptargetutilization", "0.75");
-  property_set("dalvik.vm.heapminfree", is2GB() ? "2m" : "512k");
-  property_set("dalvik.vm.heapmaxfree", "8m");
-  property_set("ro.vendor.qti.sys.fw.bg_apps_limit", is2GB() ? "17" : "9");
-}
-
 void vendor_load_properties()
 {
-    set_device_dalvik_properties();
-    init_alarm_boot_properties();
+
+  if (is2GB())
+  {
+	   property_set("ro.config.max_starting_bg", "8");
+	   property_set("dalvik.vm.heapstartsize", "8m");
+	   property_set("dalvik.vm.heapgrowthlimit", "128m");
+	   property_set("dalvik.vm.heapsize", "256m");
+	   property_set("dalvik.vm.heaptargetutilization", "0.75");
+	   property_set("dalvik.vm.heapminfree", "2m");
+	   property_set("dalvik.vm.heapmaxfree", "8m");
+
+       // Cached apps limit
+       property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "17");
+  }
+  else
+  {
+	   property_set("ro.config.low_ram", "true");
+	   property_set("persist.sys.force_highendgfx", "true");
+	   property_set("ro.config.max_starting_bg", "6");
+	   property_set("dalvik.vm.heapstartsize", "5m");
+	   property_set("dalvik.vm.heapgrowthlimit", "64m");
+	   property_set("dalvik.vm.heapsize", "128m");
+	   property_set("dalvik.vm.heaptargetutilization", "0.75");
+	   property_set("dalvik.vm.heapminfree", "2m");
+	   property_set("dalvik.vm.heapmaxfree", "8m");
+
+       // Cached apps limit
+       property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "9");
+  }
+
+  init_alarm_boot_properties();
 }
+
+
+
