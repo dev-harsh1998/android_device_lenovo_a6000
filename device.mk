2d695b2b47968dfb0e7a677cc2aff4fd407fa9c9
Jaroslav Zviezda <acroreiser@gmail.com>
2020-10-02 21:31:53 +0300

    a6010: build software Gatekeeper

--- device.mk
+++ device.mk
@@ -274,7 +274,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
-    android.hardware.authsecret@1.0-service
+    android.hardware.authsecret@1.0-service \
+    android.hardware.gatekeeper@1.0-service.software
 
 # Doze
 PRODUCT_PACKAGES += \
